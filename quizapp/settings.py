72ab3a3bdc903a2be9bf885c85c259e46ebc07c0
legendajar <harshsolanki9898@gmail.com>
2024-03-31 06:44:57 +0530

    Update ALLOWED_HOSTS and vercel.json configuration

--- quizapp/settings.py
+++ quizapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^#mmee7xw^gl1l5glvi8^2+81_qmj8+xo9#xsa)*7!!4_1t1$i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 
 
 # Application definition
